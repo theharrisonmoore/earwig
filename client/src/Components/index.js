967d514bc0cfabc6fa491f562ef96b104bed0f5a
Ramyalshurafa <ramyshurafa@hotmail.com>
2019-04-03 09:21:20 +0300

    clean up client-side

--- client/src/Components/index.js
+++ client/src/Components/index.js
@@ -1,20 +1,17 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 
-import HelloWorld from "./Pages/HelloWorld";
-
 export default function index(props) {
   return (
     <>
       <Switch>
-        <Route exact path="/hello" component={HelloWorld} />
         {/* 404 Error Page -need to be created */}
-        <Route component={A} />
+        <Route component={PageNotFound} />
       </Switch>
     </>
   );
 }
 
-function A() {
+function PageNotFound() {
   return <h1>Page Not Found</h1>;
 }
