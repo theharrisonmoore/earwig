52e84b7d58a26060a14347ef6784f62e06e2dc4a
Ramyalshurafa <ramyshurafa@hotmail.com>
2019-04-07 11:34:15 +0300

    comments schema

---
+++ server/database/models/comment.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+const { ObjectId } = Schema.Types;
+
+const commentSchema = new Schema({
+  organization: {
+    type: ObjectId,
+    ref: "organizations",
+  },
+  question: {
+    type: ObjectId,
+    ref: "questions",
+  },
+  user: {
+    type: ObjectId,
+    ref: "users",
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  timestamps: true,
+});
+
+module.exports = mongoose.model("comments", commentSchema);
