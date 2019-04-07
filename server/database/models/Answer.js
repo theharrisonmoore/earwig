d80e9300ebccdea9f327979eb1837a6b28f17298
Ramyalshurafa <ramyshurafa@hotmail.com>
2019-04-07 14:58:07 +0300

    Answer schema

---
+++ server/database/models/Answer.js
@@ -0,0 +1,29 @@
+
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+const { ObjectId, Mixed } = Schema.Types;
+
+const answerSchema = new Schema({
+  question: {
+    type: ObjectId,
+    ref: "questions",
+  },
+  answer: {
+    type: Mixed,
+  },
+  comment: {
+    type: ObjectId,
+    ref: "questions",
+  },
+  user: {
+    type: ObjectId,
+    ref: "users",
+  },
+  review: {
+    type: ObjectId,
+    ref: "reviews",
+  },
+});
+
+module.exports = mongoose.model("answeres", answerSchema);
