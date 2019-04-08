ae8ac703da28de59068a0cb5174ab1b645a5c368
Ramyalshurafa <ramyshurafa@hotmail.com>
2019-04-08 22:02:51 +0300

    add validation middleware

--- server/router/index.js
+++ server/router/index.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
+const validation = require("./../middlewares/validation");
+
 const loginController = require("./../controllers/login");
 
 // require all the routes in this file
-router.post("/login", loginController);
+router.post("/login", validation("login"), loginController);
 module.exports = router;
