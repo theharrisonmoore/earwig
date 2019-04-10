fd2454aeba8041640ffe3928aa816f34c9638b44
Simon Dupree <dupreenator@gmail.com>
2019-04-10 11:37:01 +0100

    basic functions for react-autosuggest setup

--- client/src/Components/Pages/Search/index.js
+++ client/src/Components/Pages/Search/index.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 import { SearchWrapper } from "./Search.style";
 import axios from "axios";
+import Autosuggest from "react-autosuggest";
 
 export default class Search extends Component {
   state = {
     loaded: false,
     data: null
   };
-
+  // lifecycle
   componentDidMount() {
     axios.get("/api/search").then(organizations => {
       this.setState({
@@ -17,12 +18,36 @@ export default class Search extends Component {
     });
   }
 
+  // functions
+  // Teach Autosuggest how to calculate suggestions for any given input value.
+  getSuggestions = (value, organisationsArray) => {
+    const inputValue = value.trim().toLowerCase();
+    const inputLength = inputValue.length;
+    return inputLength === 0
+      ? []
+      : organisationsArray.filter(
+          orga => orga.name.toLowerCase().slice(0, inputLength) === inputValue
+        );
+  };
+
+  // When suggestion is clicked, Autosuggest needs to populate the input
+  // based on the clicked suggestion. Teach Autosuggest how to calculate the
+  // input value for every given suggestion.
+  getSuggestionValue = suggestion => suggestion.name;
+
+  // render suggestions
+  renderSuggestion = suggestion => <div>{suggestion.name}</div>;
+
   render() {
     console.log(this.state);
     const { loaded } = this.state;
 
     if (!loaded) return <p>loading...</p>;
 
-    return <SearchWrapper />;
+    return (
+      <SearchWrapper>
+        <Autosuggest />
+      </SearchWrapper>
+    );
   }
 }
