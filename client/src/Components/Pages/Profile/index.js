58a2ddd941af8c4133afa192892fd2c8672ac8f1
Joe Friel <joseph.s.friel@gmail.com>
2019-04-10 10:48:36 +0100

    Set up front end profile component > relates #36

---
+++ client/src/Components/Pages/Profile/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+export default class Profile extends Component {
+  render() {
+    return (
+      <div>
+        <div>Organisation fixed title to go here</div>
+        <div>Initial Header to go here</div>
+        <div>Section to give a review</div>
+        <div>Key Ratings section</div>
+        <div>Detailed Ratings section</div>
+        <div>Overall Ratings section</div>
+      </div>
+    );
+  }
+}
--- client/src/Components/index.js
+++ client/src/Components/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 
+import Profile from "./Pages/Profile";
+
 export default function index(props) {
   return (
     <>
       <Switch>
+        <Route exact path="/profile/:profileID" component={Profile} />
         {/* 404 Error Page -need to be created */}
         <Route component={PageNotFound} />
       </Switch>
