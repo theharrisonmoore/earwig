b20360318d55f93195b488e3c369ab0465d89dbe
Ramyalshurafa <ramyshurafa@hotmail.com>
2019-04-11 15:11:40 +0300

    fix private key parsing error

--- server/config.js
+++ server/config.js
@@ -1,8 +1,13 @@
+const privateKey = process.env.private_key
+  .replace(new RegExp("\\\\n", "g"), "\n")
+  .replace("\"", "");
+
+
 module.exports.cred = {
   type: process.env.type,
   project_id: process.env.project_id,
   private_key_id: process.env.private_key_id,
-  private_key: process.env.private_key.replace(/\\n/g, "\n"),
+  private_key: privateKey,
   client_email: process.env.client_email,
   client_id: process.env.client_id,
   auth_uri: process.env.auth_uri,
