0bed7ea5790ecb62ca5e6feef0c33461a5ea1528
Ramyalshurafa <ramyshurafa@hotmail.com>
2019-04-18 15:14:36 +0300

    test: testing for delete and get users route

--- server/controllers/admin/allUsers.js
+++ server/controllers/admin/allUsers.js
@@ -1,8 +1,12 @@
+const boom = require("boom");
+
 const { getAllUsers } = require("./../../database/queries/user");
 
-module.exports = ((req, res) => {
+module.exports = ((req, res, next) => {
   getAllUsers()
     .then((users) => {
       res.json(users);
+    }).catch(() => {
+      next(boom.badImplementation());
     });
 });
