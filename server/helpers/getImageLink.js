f312fcc11dfaca0ce503280c5869347afaf07552
Ramyalshurafa <ramyshurafa@hotmail.com>
2019-04-21 10:49:50 +0300

    move Google storage stuff into config file

---
+++ server/helpers/getImageLink.js
@@ -0,0 +1,28 @@
+const env2 = require("env2");
+
+env2(".env");
+const { admin } = require("./../config");
+
+
+module.exports = fileName => new Promise(async (resolve, reject) => {
+  // These options will allow temporary read access to the file
+  const options = {
+    version: "v4",
+    action: "read",
+    expires: Date.now() + 15 * 60 * 1000, // 15 minutes
+  };
+  try {
+    const bucket = admin.storage().bucket();
+
+    // Get a v4 signed URL for reading the file
+    const [url] = await bucket
+      .file(fileName)
+      .getSignedUrl(options);
+
+    return resolve(url);
+  } catch (error) {
+    return reject(error);
+  }
+
+  // [END storage_generate_signed_url_v4]
+});
