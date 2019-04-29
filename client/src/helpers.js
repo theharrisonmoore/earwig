1cd4525d1c78a4e9a2e9f145b8cd84573ddc57b1
ramy <ramyshurafa@hotmail.com>
2019-04-29 14:13:49 +0300

    create a helper to determine the device type

--- client/src/helpers.js
+++ client/src/helpers.js
@@ -85,3 +85,30 @@ export const handleLogout = () => {
     window.location = LOGIN_URL;
   });
 };
+
+export const isMobileDevice = {
+  Android: function() {
+    return navigator.userAgent.match(/Android/i);
+  },
+  BlackBerry: function() {
+    return navigator.userAgent.match(/BlackBerry/i);
+  },
+  iOS: function() {
+    return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+  },
+  Opera: function() {
+    return navigator.userAgent.match(/Opera Mini/i);
+  },
+  Windows: function() {
+    return navigator.userAgent.match(/IEMobile/i);
+  },
+  any: function() {
+    return (
+      isMobileDevice.Android() ||
+      isMobileDevice.BlackBerry() ||
+      isMobileDevice.iOS() ||
+      isMobileDevice.Opera() ||
+      isMobileDevice.Windows()
+    );
+  }
+};
