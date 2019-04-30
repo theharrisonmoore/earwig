f364a0389da6ae07d2661bf78371603fa9f99317
Joe Friel <joseph.s.friel@gmail.com>
2019-04-30 12:53:14 +0100

    Send feedback messages from client > #149

--- client/src/Components/Common/CommentSection/index.js
+++ client/src/Components/Common/CommentSection/index.js
@@ -1,4 +1,8 @@
 import React, { Component } from "react";
+import axios from "axios";
+import Swal from "sweetalert2";
+
+import { colors } from "./../../../theme";
 
 import {
   Wrapper,
@@ -11,6 +15,53 @@ import {
 import { LOGIN_URL } from "./../../../constants/naviagationUrls";
 
 export default class index extends Component {
+  state = {
+    message: ""
+  };
+
+  handleInput = event => {
+    const { value } = event.target;
+    this.setState({ message: value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    const { message } = this.state;
+
+    if (message.length > 0) {
+      axios
+        .post("/api/give-feedback", { message, page: window.location.href })
+        .then(() =>
+          Swal.fire({
+            type: "success",
+            title: "Message sent",
+            text:
+              "Thanks for getting in touch. If required, we'll get back to you via email as soon as we can.",
+            confirmButtonText: "Okay",
+            confirmButtonColor: colors.heliotrope
+          }).then(() => window.location.reload())
+        )
+        .catch(err =>
+          Swal.fire({
+            type: "error",
+            title: "Error sending message",
+            text:
+              err.response.data.error === "no credentials"
+                ? "Only logged in users are authorized to send messages. Please log in first."
+                : err.response.data.error,
+            confirmButtonText: "Close"
+          })
+        );
+    } else {
+      Swal.fire({
+        type: "error",
+        title: "Message empty",
+        text: "Please write a message before sending",
+        confirmButtonText: "Close"
+      });
+    }
+  };
+
   render() {
     const { title, isLoggedIn } = this.props;
     return (
@@ -18,8 +69,11 @@ export default class index extends Component {
         <CommentTitle>{title}</CommentTitle>
         {isLoggedIn ? (
           <>
-            <CommentBox />
-            <Button>Send</Button>
+            <CommentBox
+              placeholder="Enter your comments here..."
+              onChange={this.handleInput}
+            />
+            <Button onClick={this.handleSubmit}>Send</Button>
           </>
         ) : (
           <LogInPrompt to={LOGIN_URL}>
