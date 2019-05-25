e06a2b31596cbb1c83e1613f8f806e09830f6b0e
ramy <ramyshurafa@hotmail.com>
2019-05-25 19:26:36 +0300

    add space between  and the text Relates #249

--- client/src/Components/Pages/Signup/index.js
+++ client/src/Components/Pages/Signup/index.js
@@ -141,7 +141,7 @@ export default class Signup extends Component {
                   component={CustomCheckbox}
                 />
                 <CheckboxLabel htmlFor="checkbox">
-                  I agree to the earwig
+                  I agree to the earwig{" "}
                   <LinkSpan to="T&C">Terms of Use.</LinkSpan>
                 </CheckboxLabel>
                 <FormikErrorMessage name="checkbox" component="div" />
