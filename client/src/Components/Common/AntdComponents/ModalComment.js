3666f0b96302c1cdb10543f61eac8299a5dbe2ce
ramy <ramyshurafa@hotmail.com>
2019-05-28 06:16:57 +0300

    Change Modal button labels Relates #284

--- client/src/Components/Common/AntdComponents/ModalComment.js
+++ client/src/Components/Common/AntdComponents/ModalComment.js
@@ -55,16 +55,13 @@ class ModalComment extends React.Component {
           onOk={this.handleOk}
           onCancel={this.handleCancel}
           footer={[
-            <Button key="back" onClick={this.handleCancel}>
-              Return
-            </Button>,
             <Button
               key="submit"
               type="primary"
               loading={loading}
               onClick={this.handleOk}
             >
-              Submit
+              Save
             </Button>
           ]}
         >
