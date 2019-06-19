c169a6ea2e5448ce7fa53462bddf7d2d30515a3c
abdalsamad <abdalsamad.y.m@gmail.com>
2019-06-19 11:26:39 +0300

    Set up sentry.io on both client and server
    relates #327

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import * as Sentry from "@sentry/browser";
+
 import "./index.css";
 import App from "./App";
+
+Sentry.init({
+  dsn: "https://1f427f5f106c4253abb2bf6140aa5691@sentry.io/1485403"
+});
 
 ReactDOM.render(<App />, document.getElementById("root"));
