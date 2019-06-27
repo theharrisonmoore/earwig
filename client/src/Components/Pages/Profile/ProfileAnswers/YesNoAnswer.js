6dbbb2b7ce054435c6c0a75f9aa1e79df98606d9
Joe Friel <joseph.s.friel@gmail.com>
2019-06-27 11:25:38 +0100

    Update yes no styling

--- client/src/Components/Pages/Profile/ProfileAnswers/YesNoAnswer.js
+++ client/src/Components/Pages/Profile/ProfileAnswers/YesNoAnswer.js
@@ -4,28 +4,35 @@ import { YesNoWrapper, YesHalf, NoHalf, Comment } from "./ProfileAnswers.style";
 import { organizations } from "./../../../../theme";
 
 export default class YesNoAnswer extends Component {
-  countYesNo = (answers) => {
-
-    const yesAnswers = answers ? answers.filter(answer => answer.answer === "Yes").length : 0
-    const noAnswers = answers ? answers.filter(answer => answer.answer === "No").length : 0
-    const noPerc = noAnswers / (yesAnswers + noAnswers) * 100;
+  countYesNo = answers => {
+    const yesAnswers = answers
+      ? answers.filter(answer => answer.answer === "Yes").length
+      : 0;
+    const noAnswers = answers
+      ? answers.filter(answer => answer.answer === "No").length
+      : 0;
+    const noPerc = (noAnswers / (yesAnswers + noAnswers)) * 100;
 
     const answerObj = {
       yesCount: yesAnswers,
       noCount: noAnswers,
-      yesPercentage: Math.ceil((yesAnswers / (yesAnswers + noAnswers) * 100)),
+      yesPercentage: Math.ceil((yesAnswers / (yesAnswers + noAnswers)) * 100),
       noPercentage: noPerc
-    }
-     return answerObj;
+    };
+    return answerObj;
   };
 
   render() {
     const { question, toggleComments, category } = this.props;
 
-    const answerObj = this.countYesNo(question.answers)
+    const answerObj = this.countYesNo(question.answers);
 
     return (
-      <YesNoWrapper>
+      <YesNoWrapper
+        large={
+          question.profileText === "Happy to use this agency again overall"
+        }
+      >
         <YesHalf width={answerObj.yesPercentage}>
           <p>Yes ({answerObj.yesCount})</p>
         </YesHalf>
