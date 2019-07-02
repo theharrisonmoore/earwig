e50621d83362c8cb22145096367d384ace5f4839
abdalsamad <abdalsamad.y.m@gmail.com>
2019-07-02 17:30:24 +0300

    Check if user can edit his own review under the requied conditions relates #123

--- server/controllers/checkIfCanEdit.js
+++ server/controllers/checkIfCanEdit.js
@@ -1,14 +1,24 @@
 const boom = require("boom");
-
+const moment = require("moment");
 const { findReviewById } = require("./../database/queries/review");
 
+/**
+ * Check if the user can edit his own review under these condidtion
+ * 1. it's within 30 days of giving the review;
+ * 2. the entity has not responded; and
+ * 3. the review has not been marked as helpful by any other workers.
+ */
 module.exports = async (req, res, next) => {
   const { id: reviewId } = req.params;
   try {
     const review = await findReviewById(reviewId);
+
+    const before1Month = moment().subtract(30, "days");
+    const createdLTMonthAgo = moment(review.createdAt).isSameOrAfter(before1Month);
     const hasVotes = !!review.overallReview.votes.length;
     const hasVoiceVotes = !!review.voiceReview.votes.length;
-    if (!hasVotes || !hasVoiceVotes) {
+
+    if (!hasVotes && !hasVoiceVotes && createdLTMonthAgo) {
       return res.send({ orgId: review.organization });
     }
     return next(boom.unauthorized());
