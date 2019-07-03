cffe5b44a6f33f9a54aa87fee982c00419af5d76
Simon Dupree <dupreenator@gmail.com>
2019-07-03 13:32:16 +0200

    reviews data added

--- client/src/Components/Pages/Admin/Organizations/ModifyOrg.js
+++ client/src/Components/Pages/Admin/Organizations/ModifyOrg.js
@@ -1,29 +1,96 @@
 import React, { Component } from "react";
 import axios from "axios";
-import { Link } from "react-router-dom";
 
 // import { Table, Modal, message, Input, Icon, Button } from "antd";
 
-import { Select, Spin, message, Button, Transfer } from "antd";
-
-import OrganizationsColumns from "./OrganizationsColumns";
-
-import { routes } from "./../../../../constants/adminRoutes";
-
-// styling
+import { Select, message, Button, Transfer, Tree } from "antd";
 
 const { Option } = Select;
+const { TreeNode } = Tree;
+
+const isChecked = (selectedKeys, eventKey) => {
+  return selectedKeys.indexOf(eventKey) !== -1;
+};
+
+const generateTree = (treeNodes = [], checkedKeys = []) => {
+  return treeNodes.map(({ children, ...props }) => (
+    <TreeNode {...props} disabled={checkedKeys.includes(props.key)}>
+      {generateTree(children, checkedKeys)}
+    </TreeNode>
+  ));
+};
+
+const TreeTransfer = ({ dataSource, targetKeys, ...restProps }) => {
+  const transferDataSource = [];
+  function flatten(list = []) {
+    list.forEach(item => {
+      transferDataSource.push(item);
+      flatten(item.children);
+    });
+  }
+  flatten(dataSource);
+
+  return (
+    <Transfer
+      {...restProps}
+      targetKeys={targetKeys}
+      dataSource={transferDataSource}
+      className="tree-transfer"
+      render={item => item.title}
+      showSelectAll={false}
+    >
+      {({ direction, onItemSelect, selectedKeys }) => {
+        if (direction === "left") {
+          const checkedKeys = [...selectedKeys, ...targetKeys];
+          return (
+            <Tree
+              blockNode
+              checkable
+              checkStrictly
+              defaultExpandAll
+              checkedKeys={checkedKeys}
+              onCheck={(
+                _,
+                {
+                  node: {
+                    props: { eventKey }
+                  }
+                }
+              ) => {
+                onItemSelect(eventKey, !isChecked(checkedKeys, eventKey));
+              }}
+              onSelect={(
+                _,
+                {
+                  node: {
+                    props: { eventKey }
+                  }
+                }
+              ) => {
+                onItemSelect(eventKey, !isChecked(checkedKeys, eventKey));
+              }}
+            >
+              {generateTree(dataSource, targetKeys)}
+            </Tree>
+          );
+        }
+      }}
+    </Transfer>
+  );
+};
 
 export default class AllOrganizations extends Component {
   state = {
     data: [],
-    value: [],
+
     fetching: false,
     dropDownSelection: [],
     reviewsTargetKeys: [],
     reviewsSelection: [],
     reviewsData: [],
-    selectedKeys: []
+
+    treeData: [],
+    targetKeys: []
   };
 
   componentDidMount() {
@@ -36,6 +103,7 @@ export default class AllOrganizations extends Component {
     }
   }
 
+  // gets all organisations
   fetchData = () => {
     const { category } = this.props;
     axios
@@ -51,6 +119,7 @@ export default class AllOrganizations extends Component {
       });
   };
 
+  // renders input dropdowns
   renderSelectField = (placeholder, orgArray) => (
     <Select
       showSearch
@@ -70,18 +139,34 @@ export default class AllOrganizations extends Component {
     </Select>
   );
 
+  // puts selected organisations into state
   setOrgs = e => {
-    // const obj = {key: e, content: this.state.data[e]}
-    const obj = { key: this.state.data[e]._id, org: this.state.data[e] };
+    // const orgDetails = {
+    //   key: this.state.data[e]._id,
+    //   title: `${this.state.data[e].name} - ${this.state.data[e].category} - ${this.state.data[e]._id}`
+    // };
 
+    // !! need to not concat it but exchange it every time or re-select
     this.setState({
-      dropDownSelection: [...this.state.dropDownSelection, obj]
+      dropDownSelection: [...this.state.dropDownSelection, this.state.data[e]]
     });
   };
 
+  // fills transfer data (reviews, comments)
   handleClick = async () => {
-    const orgID1 = this.state.dropDownSelection[0].key;
-    const orgID2 = this.state.dropDownSelection[1].key;
+    // const { dropDownSelection } = this.state;
+    // if (dropDownSelection.length === 2) {
+    //   this.setState({
+    //     targetKeys: [
+    //       ...this.state.targetKeys,
+    //       this.state.dropDownSelection[this.state.dropDownSelection.length - 1]
+    //         .key
+    //     ]
+    //   });
+    // }
+
+    const orgID1 = this.state.dropDownSelection[0]._id;
+    const orgID2 = this.state.dropDownSelection[1]._id;
 
     // get reviews
     const apiCall1 = () =>
@@ -90,7 +175,7 @@ export default class AllOrganizations extends Component {
       axios.get(`/api/admin/organizations/reviews/${orgID2}`);
     const allReviews = [];
 
-    // update review state
+    // update reviews state
     axios
       .all([apiCall1(), apiCall2()])
       .then(result => {
@@ -98,31 +183,55 @@ export default class AllOrganizations extends Component {
           allReviews.push(result[i].data);
         }
 
+        // edit reviews state
         const reviewsTargetKeys = [];
 
         const reviewsSelection = [];
+        const reviewChildren = [];
 
+        // length = 2
         for (let i = 0; i < allReviews.length; i++) {
-          const reviewsObj = {
-            key: i,
-            content: allReviews[i]
-          };
+          console.log("i", i);
+          console.log("allReviews[i", allReviews[i]);
+          console.log("allReviews[i].length", allReviews[i].length);
 
-          if (i === 0) {
-            reviewsTargetKeys.push(reviewsObj.key);
+          // check if there are reviews for that company
+          if (allReviews[i].length > 0) {
+            const reviews = {
+              key: i,
+              title: `reviews ${this.state.dropDownSelection[0]._id}`,
+              children: reviewChildren
+            };
+            console.log("full reviews", reviews);
+            // fill childrens array with single reviews
+            for (let j = 0; j < allReviews[i].length; j++) {
+              const singleReview = {
+                key: allReviews[i][j]._id,
+                title: allReviews[i][j]._id
+              };
+              console.log("singleReview", singleReview);
+              reviewChildren.push(singleReview);
+            }
+            reviewsSelection.push(reviews);
           }
-          reviewsSelection.push(reviewsObj);
+          if (allReviews[i].length === 0) {
+            const noReviews = {
+              key: i,
+              title: "no reviews yet"
+            };
+            console.log("no review", noReviews);
+            reviewsSelection.push(noReviews);
+          }
+          console.log("reviews", reviewsSelection);
+          // return reviewsSelection;
         }
-        this.setState({ reviewsSelection, reviewsTargetKeys });
+        // set state
+        this.setState({
+          treeData: reviewsSelection
+          // targetKeys: [...this.state.targetKeys, reviewsTargetKeys]
+        });
       })
       .catch(err => console.log(err));
-
-    // set reviews as transfer elements
-
-    // const oriTargetKeys = orgSelection.map(e => e);
-    // console.log(oriTargetKeys);
-
-    // this.setState({ targetKeys: [...this.state.targetKeys, oriTargetKeys] });
   };
 
   handleChange = (targetKeys, direction, moveKeys) => {
@@ -145,12 +254,15 @@ export default class AllOrganizations extends Component {
       // value: item.content
     };
   };
-
+  onChange = targetKeys => {
+    console.log("Target Keys:", targetKeys);
+    this.setState({ targetKeys });
+  };
   render() {
     // const { category } = this.props;
     const { data } = this.state;
 
-    // console.log(this.state);
+    console.log("treeData", this.state.treeData);
 
     return (
       <div>
@@ -161,21 +273,26 @@ export default class AllOrganizations extends Component {
 
         <Button onClick={this.handleClick}>click</Button>
         {/* review transfer section */}
-        {this.state.reviewsTargetKeys.length > 0 && (
-          <Transfer
-            dataSource={this.state.reviewsSelection}
-            targetKeys={this.state.reviewsTargetKeys}
-            // titles={[
-            //   `${this.state.orgSelection[0].org.name}`,
-            //   `${this.state.orgSelection[1].org.name}`
-            // ]}
-            listStyle={{
-              width: 300,
-              height: 300
-            }}
-            render={this.renderReview}
-            // rowKey={record => record.uid}
+        {this.state.treeData.length > 0 && (
+          <TreeTransfer
+            dataSource={this.state.treeData}
+            targetKeys={this.state.targetKeys}
+            onChange={this.onChange}
           />
+          // <Transfer
+          //   dataSource={this.state.reviewsSelection}
+          //   targetKeys={this.state.reviewsTargetKeys}
+          //   // titles={[
+          //   //   `${this.state.orgSelection[0].org.name}`,
+          //   //   `${this.state.orgSelection[1].org.name}`
+          //   // ]}
+          //   listStyle={{
+          //     width: 300,
+          //     height: 300
+          //   }}
+          //   render={this.renderReview}
+          //   // rowKey={record => record.uid}
+          // />
         )}
       </div>
     );
