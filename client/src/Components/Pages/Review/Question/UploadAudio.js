5ed5da672b19abb5ed142a0f1f2492e5c62a0bd0
ramy <ramyshurafa@hotmail.com>
2019-07-23 15:42:37 +0300

    :=

--- client/src/Components/Pages/Review/Question/UploadAudio.js
+++ client/src/Components/Pages/Review/Question/UploadAudio.js
@@ -4,12 +4,8 @@ import { message } from "antd";
 import { VoiceWrapper, VoiceIconWrapper, StopIcon } from "./Question.style";
 import Icon from "./../../../Common/Icon/Icon";
 
-// if the browser doesn't support MediaRecorder
 // use the polyfill
-if (window.MediaRecorder == null) {
-  // safari polyfill
-  window.MediaRecorder = require("audio-recorder-polyfill");
-}
+window.MediaRecorder = require("audio-recorder-polyfill");
 
 class UploadAudio extends Component {
   state = {
