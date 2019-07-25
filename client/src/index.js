9be6a5aa87470ef683741a973689710a14dda772
am <abdalsamad.y.m@gmail.com>
2019-07-25 15:19:30 +0300

    Enable 3rd party apps in production only

--- client/src/index.js
+++ client/src/index.js
@@ -11,17 +11,19 @@ import LogRocket from "logrocket";
 import setupLogRocketReact from "logrocket-react";
 import ReactGA from "react-ga";
 
-Sentry.init({
-  dsn: "https://1f427f5f106c4253abb2bf6140aa5691@sentry.io/1485403"
-});
+if (process.env.NODE_ENV === "production") {
+  Sentry.init({
+    dsn: "https://1f427f5f106c4253abb2bf6140aa5691@sentry.io/1485403"
+  });
 
-// set up logrocket
-LogRocket.init("5aud6s/earwig");
-setupLogRocketReact(LogRocket);
+  // set up logrocket
+  LogRocket.init("5aud6s/earwig");
+  setupLogRocketReact(LogRocket);
 
-// set up analytics
-ReactGA.initialize("UA-141501764-1");
-ReactGA.pageview(window.location.pathname + window.location.search);
+  // set up analytics
+  ReactGA.initialize("UA-141501764-1");
+  ReactGA.pageview(window.location.pathname + window.location.search);
+}
 
 ReactDOM.render(
   <ErrorBoundry>
