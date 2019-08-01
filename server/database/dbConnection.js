a6bc76138df4cad00f855ba853f2e4424469d888
Simon Dupree <dupreenator@gmail.com>
2019-08-01 12:48:11 +0200

    updated dbConnection

--- server/database/dbConnection.js
+++ server/database/dbConnection.js
@@ -8,7 +8,7 @@ let mongoURI = process.env.MONGO_URI;
 
 if (process.env.NODE_ENV === "test") {
   // change mongoURI to testing database URI
-  mongoURI = process.env.MONGO_URI_TEST;
+  mongoURI = process.env.MONGOURI_TEST;
 }
 
 // create DB connection
