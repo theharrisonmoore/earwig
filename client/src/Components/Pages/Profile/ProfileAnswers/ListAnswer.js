cf376eaef3bdcfb2050ee71c67fb7b9bf76351ef
am <abdalsamad.y.m@gmail.com>
2019-08-04 11:44:51 +0300

    style: style links for agency with the right color in the payroll profile
    relates #611

--- client/src/Components/Pages/Profile/ProfileAnswers/ListAnswer.js
+++ client/src/Components/Pages/Profile/ProfileAnswers/ListAnswer.js
@@ -69,7 +69,10 @@ export default class ListAnswer extends Component {
               }
               key={index}
             >
-              <Link to={`/profile/${answer.answer._id}`}>
+              <Link
+                to={`/profile/${answer.answer._id}`}
+                style={{ color: "inherit" }}
+              >
                 {answer.answer.name}
               </Link>
             </ListItem>
