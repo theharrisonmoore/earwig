5e6acc72aca9e7096f12f263f8dd2dc9433c9930
am <abdalsamad.y.m@gmail.com>
2019-08-08 10:39:15 +0300

    change sentry api key

--- client/src/index.js
+++ client/src/index.js
@@ -13,18 +13,18 @@ import ErrorBoundry from "./Components/Common/ErrorBoundry";
 // third party apps
 if (process.env.NODE_ENV === "production") {
   Sentry.init({
-    dsn: "https://1f427f5f106c4253abb2bf6140aa5691@sentry.io/1485403"
+    dsn: "https://2b089a5f39714cd3bc694a5b2484302b@sentry.io/1524934"
   });
 
   // set up logrocket
   LogRocket.init("5aud6s/earwig");
   setupLogRocketReact(LogRocket);
 
-  mixpanel.init("6e556a55e4e9c20a0ecbc15e28fc00d8");
   // set up analytics
   ReactGA.initialize("UA-141501764-1");
   ReactGA.pageview(window.location.pathname + window.location.search);
 }
+mixpanel.init("6e556a55e4e9c20a0ecbc15e28fc00d8");
 
 ReactDOM.render(
   <ErrorBoundry>
