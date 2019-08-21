544cfcca7c8764b19888111db439affe547beae0
amusameh <abdalsamad.y.m@gmail.com>
2019-08-21 16:51:33 +0300

    fix type comming => coming

--- client/src/Components/Pages/Welcome/index.js
+++ client/src/Components/Pages/Welcome/index.js
@@ -120,7 +120,7 @@ export default class Welcome extends Component {
                   <ButtonText>
                     <Text>
                       Ask workers a question
-                      <ComingSoon>(comming soon)</ComingSoon>
+                      <ComingSoon>(coming soon)</ComingSoon>
                     </Text>
                     {!verified && !awaitingReview && (
                       <HintText>(Click here to get verified first)</HintText>
@@ -145,7 +145,7 @@ export default class Welcome extends Component {
                   />
                   <ButtonText>
                     <Text>
-                      Find a job <ComingSoon>(comming soon)</ComingSoon>
+                      Find a job <ComingSoon>(coming soon)</ComingSoon>
                     </Text>
 
                     {!verified && !awaitingReview && (
