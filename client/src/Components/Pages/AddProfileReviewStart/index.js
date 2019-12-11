5009b0e0d1e3038ec7e4b3c3662e39b5345f354b
ramy <ramyshurafa@hotmail.com>
2019-12-11 12:20:04 +0200

    create new profile when giving review

--- client/src/Components/Pages/AddProfileReviewStart/index.js
+++ client/src/Components/Pages/AddProfileReviewStart/index.js
@@ -1,13 +1,9 @@
 import React, { Component } from "react";
 
-import axios from "axios";
-import { Modal, Spin } from "antd";
+import { Spin } from "antd";
+import { Link } from "react-router-dom";
 
 import Layout from "../../Common/Layout";
-
-// constants
-import { API_ADD_ORGANIZATION_URL } from "../../../apiUrls";
-import { CONTACT_URL } from "../../../constants/naviagationUrls";
 
 // common component
 import Button from "../../Common/Button";
@@ -29,50 +25,50 @@ export default class AddProfileReviewStart extends Component {
     this.props.history.push("/search");
   };
 
-  createNewOrg = () => {
-    this.setState({ isLoading: true }, () => {
-      const { match: { params: { name, category } } = {} } = this.props;
+  // createNewOrg = () => {
+  //   this.setState({ isLoading: true }, () => {
+  //     const { match: { params: { name, category } } = {} } = this.props;
 
-      const newOrg = { name, category };
-      axios
-        .post(API_ADD_ORGANIZATION_URL, newOrg)
-        .then(({ data }) => {
-          this.setState({ isLoading: false });
+  //     const newOrg = { name, category };
+  //     axios
+  //       .post(API_ADD_ORGANIZATION_URL, newOrg)
+  //       .then(({ data }) => {
+  //         this.setState({ isLoading: false });
 
-          const state = {
-            name,
-            category,
-            needsVerification: true,
-          };
+  //         const state = {
+  //           name,
+  //           category,
+  //           needsVerification: true,
+  //         };
 
-          const { _id: orgId } = data;
-          this.props.history.push({
-            pathname: `/organization/${orgId}/review`,
-            state,
-          });
-        })
-        .catch(() => {
-          this.setState({ isLoading: false });
-          Modal.error({
-            type: "error",
-            title: "Oops...",
+  //         const { _id: orgId } = data;
+  //         this.props.history.push({
+  //           pathname: `/organization/${orgId}/review`,
+  //           state,
+  //         });
+  //       })
+  //       .catch(() => {
+  //         this.setState({ isLoading: false });
+  //         Modal.error({
+  //           type: "error",
+  //           title: "Oops...",
 
-            content: (
-              <div>
-                <p>
-                  {name} already exists. Please contact us directly with your
-                  request.
-                </p>
-                <a href={CONTACT_URL}>Contact</a>
-              </div>
-            ),
-          });
-        });
-    });
-  };
+  //           content: (
+  //             <div>
+  //               <p>
+  //                 {name} already exists. Please contact us directly with your
+  //                 request.
+  //               </p>
+  //               <a href={CONTACT_URL}>Contact</a>
+  //             </div>
+  //           ),
+  //         });
+  //       });
+  //   });
+  // };
 
   render() {
-    const { match: { params: { name } } = {} } = this.props;
+    const { match: { params: { name, category } } = {} } = this.props;
 
     const { isLoading } = this.state;
 
@@ -83,14 +79,16 @@ export default class AddProfileReviewStart extends Component {
             <MainDiv>
               <HeadlineDiv>
                 <H2>Give {name} a review to add it to the earwig database</H2>
-                <div style={{ textAlign: "center" }}>
+                <Link
+                  style={{ textAlign: "center" }}
+                  to={`/organization/review/new/${category}/${name}`}
+                >
                   <Button
                     margin="2rem auto"
                     styleType="primary"
                     text="Start your review"
-                    onClick={this.createNewOrg}
                   />
-                </div>
+                </Link>
 
                 <Button
                   margin="0 auto"
