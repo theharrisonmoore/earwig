f17959f1b9f0b2c03b9b6ac13bc93cf5af51eafd
ramy <ramyshurafa@hotmail.com>
2019-12-15 11:04:18 +0200

    Fix #887

--- client/src/Components/Pages/PreReview/index.js
+++ client/src/Components/Pages/PreReview/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import { message } from "antd";
 
 import Layout from "../../Common/Layout";
 import Button from "../../Common/Button";
@@ -14,17 +16,42 @@ import {
 } from "./PreReview.style";
 
 export default class PreReview extends Component {
-  render() {
-    const { match, location, history } = this.props;
-    const { state: locationState } = location;
+  state = {
+    category: null,
+    name: null,
+  };
+
+  async componentDidMount() {
+    const { match, location } = this.props;
+    const { state: locationState = {} } = location;
     const { name, category } = locationState;
     const { orgId } = match.params;
 
     // TODO if one of these are missed should send a request to backend to load info
-    if (!name || !category || !orgId) {
-      history.goBack();
-      return null;
+    if (!name || !category) {
+      axios
+        .get(`/api/organizations/${orgId}`)
+        .then(({ data: { organisation } }) => {
+          const { category: _category, name: _name } = organisation;
+          this.setState({ category: _category, name: _name });
+        })
+        .catch(err => {
+          const error =
+            err.response && err.response.data && err.response.data.error;
+          message.error(error || "Something went wrong");
+        });
+    } else {
+      this.setState({ category, name });
     }
+  }
+
+  render() {
+    const { match, history, location } = this.props;
+    const { state: locationState = {} } = location;
+    const { name: _name, category: _category } = locationState;
+    const { category = _category, name = _name } = this.state;
+
+    const { orgId } = match.params;
 
     return (
       <Layout type="center" maxWidth="57rem">
