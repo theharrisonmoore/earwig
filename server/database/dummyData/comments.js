f5fecd0480726860361bab2754c8c838405fe22c
ramy <ramyshurafa@hotmail.com>
2020-01-02 10:28:43 +0200

    fix tests

--- server/database/dummyData/comments.js
+++ server/database/dummyData/comments.js
@@ -2,6 +2,7 @@ const Question = require("../models/Question");
 const Organization = require("../models/Organization");
 const User = require("../models/User");
 const Comment = require("../models/Comment");
+const Review = require("../models/Review");
 
 module.exports = async () => {
   const agencyQuestions = await Question.find({ category: "agency" }).sort({ number: 1 });
@@ -9,6 +10,8 @@ module.exports = async () => {
   const agencies = await Organization.find({ category: "agency" });
 
   const users = await User.find({ verified: true, isAdmin: false });
+  const review1 = await Review.findOne({ user: users[0] });
+  const review2 = await Review.findOne({ user: users[1] });
 
 
   const comments = [
@@ -17,12 +20,14 @@ module.exports = async () => {
       question: agencyQuestions[0],
       user: users[0],
       text: "The contract was misleading and tricky",
+      review: review1,
     },
     {
       organization: agencies[0],
       question: agencyQuestions[0],
       user: users[1],
       text: "This is a shity agency with a confusing contract. I wouldn't work with them again",
+      review: review2,
     },
   ];
   return Comment.create(comments);
