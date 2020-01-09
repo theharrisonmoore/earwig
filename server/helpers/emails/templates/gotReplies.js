de7b4d3718a42fd99e063244002c4a59c13d768e
ramy <ramyshurafa@hotmail.com>
2020-01-09 14:15:19 +0200

    fix email style

--- server/helpers/emails/templates/gotReplies.js
+++ server/helpers/emails/templates/gotReplies.js
@@ -8,16 +8,18 @@ module.exports = ({ orgId, recipientEmail }) => {
     <p style="font-weight: 700;">
       Hey, a worker just replied to your review on earwig!
     </p>
-    <a href="${config.server.domain}/profile/${orgId}" target="_blank" rel="noreferrer" rel="noopener"
+    <p>
+      <a href="${config.server.domain}/profile/${orgId}" target="_blank" rel="noreferrer" rel="noopener"
       style="color: #8c6bfc; font-weight: 900;">
       Go to your review
-    </a>
+      </a>
+    </p>
 
     <p>
       By the way, we know getting too many emails can be annoying. 
       But please bear with us, we don't have an app yet so we can only use email to tell you about important developments. 
       We're working as hard as we can to build an app so we don't have to send emails. 
-      We'll let you know (by email ;) when we've done it.     
+      We'll let you know (by email) when we've done it.     
     </p>
 
     <p style="margin-bottom: 0;">Thanks for your patience,</p>
