39919ab3dd73eda9b1ac31d285e8267a909a556b
ramy <ramyshurafa@hotmail.com>
2020-01-14 11:00:38 +0200

    check production env

--- client/src/FBPixel.js
+++ client/src/FBPixel.js
@@ -1,6 +1,15 @@
 import ReactPixel from "react-facebook-pixel";
 
-const FBPixelInit = () => ReactPixel.init("577033549720607");
-const FBPixelTrack = (event, data) => ReactPixel.track(event, data);
+const FBPixelInit = () => {
+  if (process.env.NODE_ENV === "production") {
+    ReactPixel.init("577033549720607");
+  }
+};
+
+const FBPixelTrack = (event, data) => {
+  if (process.env.NODE_ENV === "production") {
+    ReactPixel.track(event, data);
+  }
+};
 
 export { FBPixelInit, FBPixelTrack };
