64ad17124d34ba0c63b88065eaca4b332f8bd975
dupreesi <dupreenator@gmail.com>
2020-01-14 11:21:38 +0100

    updated link in approval email

--- server/helpers/emails/templates/approvalEmail.js
+++ server/helpers/emails/templates/approvalEmail.js
@@ -1,11 +1,12 @@
 const mailer = require("../mailer");
 const config = require("../../../config");
 
-module.exports = (to) => {
+module.exports = to => {
   const { domain } = config.server;
   const giveReviewLink = `${domain}/search/review`;
-  const viewProfileLink = `${domain}/search/profile`;
+  // const viewProfileLink = `${domain}/search/profile`;
   const introLink = `${domain}/intro`;
+  const welcomeLink = `${domain}/`;
 
   const html = `
   <div style="text-align: center;">
@@ -21,25 +22,25 @@ module.exports = (to) => {
         <li>Asking and answering questions</li>
         <li>Inviting other workers to earwig</li>
         <li>Searching jobs</li>
-        <li>Plus, other stuff you might find helpful</li>      
+        <li>Plus, other stuff you might find helpful</li>
       </ul>
 
       <p>When you give a review, you help to create awareness, improve working conditions and help other workers feel more secure. Get started and give a review now!</p>
 
-      
+
       <div style="text-align: center;">
         <a href="${giveReviewLink}" style="display: inline-block; padding: 0.5rem 1rem; background: #8c6bfc; color: white; font-size: 20px; font-weight: 900; border-radius: 10px; box-shadow: 0px 5px 11px 1px #9e9e9e7d; text-decoration: none;">Give a review</a>
-      </div>  
+      </div>
 
-      <p>Or, why not <a href="${viewProfileLink}">read reviews and ratings</a> others have already given.</p>
+      <p>Or, why not <a href="${welcomeLink}">read reviews and ratings</a> others have already given.</p>
       </br>
       <p style="font-weight: 700;">Thanks for choosing to be part of the earwig worker community.</p>
       </br>
       <p style="margin-bottom: 0;">Have a good day,</p>
       <p style="margin-bottom: 0;">Harrison</p>
       <p style="margin-bottom: 0;">Sparky and founder</p>
-    </div>  
-  </div>  
+    </div>
+  </div>
 `;
 
   const { email } = config;
